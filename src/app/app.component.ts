7229bfacbb63dea43b62ee9745ee8dcaf484a6e1
Buchner72 <franz.buchner@gmx.at>
2019-04-16 09:31:42 +0200

    Vorgensweise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,7 @@ export class AppComponent {
 }
 
 
-//Vorgensweise
-//1.Personen anlegen
+// Vorgensweise
+// 1.Personen anlegen (Struktur anlegen)
+// 2.Web API mit Personen erstellen (Visual Studio)
+// 3.Auf WebAPI zugreifen (get, put, post usw )
