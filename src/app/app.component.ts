2828b7455403e985be0445a554c2b722fea435ad
Buchner72 <franz.buchner@gmx.at>
2019-04-20 13:31:01 +0200

    commit VR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 // 1.Personen anlegen (Struktur anlegen)
 // 2.Web API mit Personen erstellen (Visual Studio)
 // 3.Auf WebAPI zugreifen (get, put, post usw )
-// Oh mien Gott
+// Oh mien Gott!
 
 // Jaja erst  mit "npm install -g @angular/cl"  auch das CLI installiern, und es funktioniert auch "ng serve" und nur "npm start"
 
