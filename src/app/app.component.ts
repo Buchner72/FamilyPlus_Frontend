bc8a3a6bb309fc314bbc9f2462f812f9a25f0274
Buchner72 <franz.buchner@gmx.at>
2019-04-28 11:49:55 +0200

    Mit Anleitung für Angular Projekt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,17 @@ export class AppComponent {
   title = 'FamilyPlus';
 }
 
-// Vorgensweise
+// Weitere Vorgensweise
 // 1.Personen anlegen (Struktur anlegen)
 // 2.Web API mit Personen erstellen (Visual Studio)
 // 3.Auf WebAPI zugreifen (get, put, post usw )
 // Oh mien Gott!!!
 
 // Jaja erst  mit "npm install -g @angular/cl"  auch das CLI installiern, und es funktioniert auch "ng serve" und nur "npm start"
+
+// Wichtig wenn Repository von GitHub neu geclont wird ausgesheckt.
+// 1. Node installieren ->        https://nodejs.org/en/
+// 2. Angular CLI installieren -> https://cli.angular.io/
+// 3. Bootstrap 4 Styling ->      https://codeburst.io/getting-started-with-angular-7-and-bootstrap-4-styling-6011b206080
+
 
